f41a2a23b3789e2c719e4fe3afeb7307320c82d9
Rajrohith <rajrohith902@gmail.com>
2025-05-21 14:17:10 +0530

    mark 2

--- bank_app/urls.py
+++ bank_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('banking_app.urls')),   
+    path('api/', include('bank_app.urls')),   
 ]
